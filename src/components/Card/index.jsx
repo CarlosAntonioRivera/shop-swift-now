10b783c4ebe4232169b36dff441e7c878e612452
Carlos Rivera <carlos.rivera0904@gmail.com>
2024-02-17 15:20:34 -0500

    Consumo de la Fake Store API para pintar las cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,17 @@
-const Card = () => {
+const Card = (data) => {
+  const category =
+    data.data.category[0].toUpperCase() + data.data.category.slice(1);
+
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
-          Electronics
+          {category}
         </span>
         <img
-          className='w-full h-full object-cover rounded-lg'
-          src='https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-          alt='headphones'
+          className='w-full h-full object-contain rounded-lg'
+          src={data.data.image}
+          alt={data.data.title}
         />
         <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2'>
           +
@@ -16,8 +19,10 @@ const Card = () => {
       </figure>
 
       <p className='flex justify-between'>
-        <span className='font-light'>Headphones</span>
-        <span className='text-lg font-medium'>$300</span>
+        <span className='text-sm font-light line-clamp-2 w-40'>
+          {data.data.title}
+        </span>
+        <span className='text-lg font-medium'>${data.data.price}</span>
       </p>
     </div>
   );
