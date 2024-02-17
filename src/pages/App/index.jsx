cebed942940d0574b4b2ee491aeb6e470cddf9ce
Carlos Rivera <carlos.rivera0904@gmail.com>
2024-02-17 06:16:52 -0500

    Creación de componente Navbar

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder';
 import MyOrders from '../MyOrders';
 import SignIn from '../SignIn';
 import NotFound from '../NotFound';
+import NavBar from '../../components/Navbar';
 import './App.css';
 
 const AppRoutes = () => {
@@ -24,6 +25,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <NavBar />
     </BrowserRouter>
   );
 };
