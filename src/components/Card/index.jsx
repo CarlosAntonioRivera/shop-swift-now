481eb9e9c285db4cca65cf2b75dd61534e95d911
Carlos Rivera <carlos.rivera0904@gmail.com>
2024-08-22 08:59:38 -0500

    Modified the design of the 'Card' view and changed the font to Quicksand

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,12 @@
-import { PlusCircleIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
+import { PlusCircleIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../context';
 import { NavLink } from 'react-router-dom';
 
 const Card = (data) => {
   // Card . Change first letter to uppercase
-  const category =
-    data.data.category[0].toUpperCase() + data.data.category.slice(1);
+  const category = data.data.category.toUpperCase();
 
   // Context .
   const context = useContext(ShoppingCartContext);
@@ -29,56 +29,70 @@ const Card = (data) => {
     context.closeProductDetail();
   };
 
-  const renderIcon = (id) => {
+  const renderButton = (id) => {
     if (!isUserSignOut) {
       const isInCart =
         context.cartProducts.filter((product) => product.id === id).length > 0;
 
       if (isInCart) {
         return (
-          <CheckCircleIcon className='h-6 w-6 text-white absolute bottom-0 right-0 m-1 bg-black rounded-full' />
+          <button className='flex items-center justify-center w-full h-10 border border-black bg-white rounded-lg'>
+            <CheckCircleIcon className='h-6 w-6' />
+          </button>
         );
       } else {
         return (
-          <PlusCircleIcon
-            className='h-6 w-6 absolute bottom-0 right-0 m-1'
+          <button
+            className='flex items-center w-full h-10 px-4 border border-black bg-white rounded-lg'
             onClick={(event) => addProductToCart(event, data.data)}
-          />
+          >
+            <PlusCircleIcon className='h-6 w-6' />
+            <span className='font-bold text-sm w-full'>Add to Cart</span>
+          </button>
         );
       }
     } else {
       return (
-        <NavLink to={'/sign-in'}>
-          <PlusCircleIcon className='h-6 w-6 absolute bottom-0 right-0 m-1' />
+        <NavLink
+          className='w-full'
+          to={'/sign-in'}
+        >
+          <button
+            className='flex items-center w-full h-10 px-4 border border-black bg-white rounded-lg'
+            onClick={(event) => addProductToCart(event, data.data)}
+          >
+            <PlusCircleIcon className='h-6 w-6' />
+            <span className='font-bold text-sm w-full'>Add to Cart</span>
+          </button>
         </NavLink>
       );
     }
   };
 
   return (
-    <div
-      className='border cursor-pointer w-56 h-60 rounded-lg'
+    <article
+      className='border cursor-pointer w-60 h-80 rounded-lg'
       onClick={() => showProduct(data.data)}
     >
-      <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
-          {category}
-        </span>
+      <figure className='relative w-full h-3/5'>
         <img
-          className='w-full h-full object-contain rounded-lg'
+          className='w-full h-full p-2 object-contain rounded-lg'
           src={data.data.image}
           alt={data.data.title}
         />
-        {renderIcon(data.data.id)}
       </figure>
 
-      <p className='flex justify-between p-1 h-10 items-center bg-[#e5e7eb] rounded-b-lg'>
-        <span className='text-sm font-light line-clamp-2 w-40 p-1'>
-          {data.data.title}
-        </span>
-        <span className='text-lg font-medium'>${data.data.price}</span>
-      </p>
-    </div>
+      <div className='flex flex-col justify-between h-2/5 pt-1 px-3 pb-3 items-center rounded-b-lg'>
+        <span className='text-xs font-bold mb-1'>{category}</span>
+
+        <section className='flex justify-between items-center w-full'>
+          <p className='text-sm line-clamp-2 w-full p-1'>{data.data.title}</p>
+          <span className='text-lg font-bold ml-1'>${data.data.price}</span>
+        </section>
+
+        {renderButton(data.data.id)}
+      </div>
+    </article>
   );
 };
 
